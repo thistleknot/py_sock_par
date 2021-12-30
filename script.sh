1bfa052b74484221ffd65c76dfaf2497e40f9b53
Joshua Laferriere <laferrierejc@gmail.com>
2021-12-30 21:30:16 +0000

    testing dask

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ cp -f /mnt/distvol/python_user/python_user/*.ipynb /mnt/distvol/dev/code/py_sock
 cp /mnt/distvol/python_user/python_user/script.sh /mnt/distvol/dev/code/py_sock_par/
 
 cd /mnt/distvol/dev/code/py_sock_par/
-ssh -T git@github.com
+ssh -T git@github.com
 #git remote set-url origin laferrierejc@gmail.com:github.com/thistleknot/py_sock_par
 git add *
 git commit -m "testing dask"
