1a1edcb2bac3ae75b57507586292a6046af2024c
Joshua Laferriere <laferrierejc@gmail.com>
2021-12-31 04:33:17 +0000

    updating

---
+++ backup_script.sh
@@ -0,0 +1,9 @@
+cp -f /mnt/distvol/python_user/python_user/*.ipynb /mnt/distvol/dev/code/py_sock_par/
+cp /mnt/distvol/python_user/python_user/*.txt /mnt/distvol/dev/code/py_sock_par/
+cp /mnt/distvol/python_user/python_user/*.sh /mnt/distvol/dev/code/py_sock_par/
+
+cd /mnt/distvol/dev/code/py_sock_par/
+ssh -T git@github.com
+git add *
+git commit -m "updating"
+git push
