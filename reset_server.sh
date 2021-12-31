8ad27b43cb6b92e76819cb5c7d1eac8c947d6b46
Joshua Laferriere <laferrierejc@gmail.com>
2021-12-31 04:33:04 +0000

    updating

--- reset_server.sh
+++ reset_server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 #server
+array=('slurm-w01' 'slurm-w02' 'slurm-w03' 'slurm-w04')
 
-ansible -m shell -a "pkill -9 dask-scheduler" ansible; ansible -m shell -a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-scheduler >out.log 2>err.log &'" ansible; ansible -m shell -a "pkill -9 dask-worker" slurmw; for a in "${array[@]}"; do ssh $a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 4 --nthreads 1 --memory-limit=3GB >out.log 2>err.log &'"; done
+ansible -m shell -a "pkill -9 dask-scheduler" ansible; ansible -m shell -a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-scheduler >out.log 2>err.log &'" ansible; 
 
 #client
 
 ansible -m shell -a "pkill -9 dask-worker" slurmw; for a in "${array[@]}"; do ssh $a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 4 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'"; done
+
+#jupyter
+
+ansible -m shell -a "pkill -9 jupyter-lab" ansible; ssh ansible "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/jupyter-lab >out.log 2>err.log &'"
