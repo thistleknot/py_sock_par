31f6da47fd78396a36cb5057f2f710adb9fdb0c1
Joshua Laferriere <laferrierejc@gmail.com>
2022-01-01 01:40:18 +0000

    updating

--- reset_server.sh
+++ reset_server.sh
@@ -4,6 +4,11 @@
 #slurm-w01 has gpu prepended
 array=('slurm-w02' 'slurm-w03' 'slurm-w04')
 
+#jupyter
+#put in python_user's nanobash.rc
+#export LD_LIBRARY_PATH="/mnt/distvol/R/4.1.2/lib64/R/lib/"
+ansible -m shell -a "pkill -9 jupyter-lab" ansible; ssh ansible "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/jupyter-lab >out.log 2>err.log &'"
+
 ansible -m shell -a "pkill -9 dask-scheduler" ansible; ansible -m shell -a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-scheduler >out.log 2>err.log &'" ansible; 
 
 #client
@@ -11,7 +16,7 @@ ansible -m shell -a "pkill -9 dask-scheduler" ansible; ansible -m shell -a "runu
 ansible -m shell -a "pkill -9 dask-worker" slurmw; 
 #1st one is cuda
 #ssh slurm-w01 "runuser -l python_user -c 'CUDA_VISIBLE_DEVICES=0 /mnt/distvol/py39_jupyterlab/bin/dask-cuda-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
-ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --resources 'GPU=1' --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
+ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --resources 'GPU=1' --name CUDA --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
 ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
 ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
 ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'";
@@ -21,8 +26,3 @@ ssh slurm-w01 "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-
 #for a in "slurm-w01"; do ssh $a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'"; done
 #for a in "slurm-w01"; do ssh $a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 1 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'"; done
 for a in "${array[@]}"; do ssh $a "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/dask-worker tcp://192.168.3.100:8786 --nprocs 4 --nthreads 1 --memory-limit=768MB >out.log 2>err.log &'"; done
-
-#jupyter
-#put in python_user's nanobash.rc
-#export LD_LIBRARY_PATH="/mnt/distvol/R/4.1.2/lib64/R/lib/"
-ansible -m shell -a "pkill -9 jupyter-lab" ansible; ssh ansible "runuser -l python_user -c '/mnt/distvol/py39_jupyterlab/bin/jupyter-lab >out.log 2>err.log &'"
