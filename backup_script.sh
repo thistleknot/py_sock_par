92561c4774578b218902b4e127eed958f7d951af
Joshua Laferriere <laferrierejc@gmail.com>
2022-01-08 00:54:48 +0000

    updating

--- backup_script.sh
+++ backup_script.sh
@@ -1,6 +1,6 @@
-cp -f /mnt/distvol/python_user/python_user/*.ipynb /mnt/distvol/dev/code/py_sock_par/
-cp /mnt/distvol/python_user/python_user/*.txt /mnt/distvol/dev/code/py_sock_par/
-cp /mnt/distvol/python_user/python_user/*.sh /mnt/distvol/dev/code/py_sock_par/
+yes | cp -r /mnt/distvol/python_user/python_user/*.ipynb /mnt/distvol/dev/code/py_sock_par/
+yes | cp -r /mnt/distvol/python_user/python_user/*.txt /mnt/distvol/dev/code/py_sock_par/
+yes | cp -r /mnt/distvol/python_user/python_user/*.sh /mnt/distvol/dev/code/py_sock_par/
 
 cd /mnt/distvol/dev/code/py_sock_par/
 ssh -T git@github.com
